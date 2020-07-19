3ff0c5066248088c713ef9257e9222a988fd976a
--replace-all <r.carolinjeba@outlook.com>
2020-07-19 13:42:41 +0530

    Components props and PropTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Carol';
-    const loading = false;
-    const showName = true;
-
-    // const foo = () => {
-    //   return 'Carolin';
-    // }
-
-    // if (loading) {
-    //   return <h4>Loading....</h4>
-    // }
 
     return (
       <div className="App">
-        {/* <h4>{foo()}</h4> */}
-        {/* <h1>Hello {showName ? name : null}</h1> */}
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-
+        <Navbar />
       </div>
     );
   }
